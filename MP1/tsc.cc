6b05973f5a086064c9ac57a0b3f683db3297b6d9
sirija-tamu <145520439+sirija-tamu@users.noreply.github.com>
2024-09-19 22:01:41 +0000

    updating the time stamp

--- MP1/tsc.cc
+++ MP1/tsc.cc
@@ -355,10 +355,10 @@ void Client::Timeline(const std::string& username) {
         Message m;
         while (stream->Read(&m)) {
             // Display the post message received from the server with a timestamp
-	    google::protobuf::Timestamp* timestamp = new google::protobuf::Timestamp();
-	    timestamp->set_seconds(time(NULL));
-	    timestamp->set_nanos(0);
-            displayPostMessage(m.username(), m.msg(), timestamp);
+            google::protobuf::Timestamp* timestamp = new google::protobuf::Timestamp();
+            timestamp->set_seconds(time(NULL));
+            timestamp->set_nanos(0);
+            displayPostMessage(m.username(), m.msg(), timestamp.seconds());
         }
     });
 
